05110bbe4db1c226e06aada22169b6066b13734e
Jjingo W. Kisakye <jingowasaaka@gmail.com>
2021-03-29 02:34:58 +0300

    fixed registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,19 @@
 class RegistrationsController < ApplicationController
-
-def create
-    user = User.create!(
-        first_name: params['user']['first_name'],
-        last_name: params['user']['last_name'],
+    def create
+      user = User.create!(
         email: params['user']['email'],
         password: params['user']['password'],
         password_confirmation: params['user']['password_confirmation']
-    )
-
-    if user 
+      )
+  
+      if user
         session[:user_id] = user.id
         render json: {
-            status: :created
-            user: user
-
+          status: :created,
+          user: user
         }
-    else
-        render json: {status: 500}
+      else
+        render json: { status: 500 }
+      end
     end
-end
-
-end 
+  end
