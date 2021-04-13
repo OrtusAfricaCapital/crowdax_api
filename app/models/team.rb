8aed5bb842200fcc1cde8ce1e41e85132d878697
Jjingo W. Kisakye <jingowasaaka@gmail.com>
2021-04-13 12:13:59 +0300

    recreate heroku db

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
     validates :venture_id, presence: true
     validates :name, presence: true
-    validates :role, presence: true
+    #validates :role, presence: true
 end
